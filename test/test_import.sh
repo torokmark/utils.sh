64a713b2aa1cd9bc41b37894c477155924d1ef03
Mark Torok <torok.marko@gmail.com>
2017-01-29 20:57:28 +0100

    Add fix

--- test/test_import.sh
+++ test/test_import.sh
@@ -4,15 +4,15 @@ source "$IMPORT_HOME"
 
 #import "" # Error
 
-#import "./test/import/test_import_shell.sh"
+#import "/home/myuser/dev/shell_utils/test/import/test_import_shell.sh"
 #test_import_shell_func
 
-#import "./test/import/directory/*"
+#import "/home/myuser/dev/shell_utils/test/import/directory/*"
 #test_import_directory_func
 #test_import_directory_second_func
 
-import "./test/import/directory/recursive/**"
-test_import_directory_recursive_one_depth_func
-test_import_directory_recursive_one_depth_second_func
-test_import_directory_recursive_two_depth_func
-test_import_directory_recursive_two_depth_second_func
+#import "/home/myuser/dev/shell_utils/test/import/directory/recursive/**"
+#test_import_directory_recursive_one_depth_func
+#test_import_directory_recursive_one_depth_second_func
+#test_import_directory_recursive_two_depth_func
+#test_import_directory_recursive_two_depth_second_func
