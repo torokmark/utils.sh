3cbae4fd3ea4629979560d2ab55b9be6e2a0731a
Mark Torok <torok.marko@gmail.com>
2017-01-29 23:14:49 +0100

    Add tests

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 root_dir="$( pwd )"
+
+export PROJECT_HOME="$root_dir"
 export IMPORT_HOME="$root_dir/lib/import.sh"
---
+++ test/import/http/test_import_http.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+test_import_http_func() {
+  echo "test_import_http_func is called";
+}
