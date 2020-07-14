cee99a184cbf9ba98f28d293b6c5a74a3ec5b188
Torok Mark <torok.marko@gmail.com>
2020-07-14 23:28:16 +0200

    Add ANSI escape colors

--- lib/console.sh
+++ lib/console.sh
@@ -21,22 +21,33 @@
 #####################################################################
 
 
-#BLACK=$(tput setaf 0)
-RED=$(tput setaf 1)
-GREEN=$(tput setaf 2)
-YELLOW=$(tput setaf 3)
-#LIME_YELLOW=$(tput setaf 190)
-#POWDER_BLUE=$(tput setaf 153)
-#BLUE=$(tput setaf 4)
-MAGENTA=$(tput setaf 5)
-#CYAN=$(tput setaf 6)
-WHITE=$(tput setaf 7)
-#BRIGHT=$(tput bold)
-NORMAL=$(tput sgr0)
-BOLD=$(tput bold)
-#BLINK=$(tput blink)
-#REVERSE=$(tput smso)
-UNDERLINE=$(tput smul)
+if command -v tput &>/dev/null && tty -s; then
+  #BLACK=$(tput setaf 0)
+  RED=$(tput setaf 1)
+  GREEN=$(tput setaf 2)
+  YELLOW=$(tput setaf 3)
+  #LIME_YELLOW=$(tput setaf 190)
+  #POWDER_BLUE=$(tput setaf 153)
+  #BLUE=$(tput setaf 4)
+  MAGENTA=$(tput setaf 5)
+  #CYAN=$(tput setaf 6)
+  WHITE=$(tput setaf 7)
+  #BRIGHT=$(tput bold)
+  NORMAL=$(tput sgr0)
+  BOLD=$(tput bold)
+  #BLINK=$(tput blink)
+  #REVERSE=$(tput smso)
+  UNDERLINE=$(tput smul)
+else
+  RED=$(echo -en "\e[31m")
+  GREEN=$(echo -en "\e[32m")
+  YELLOW=$(echo -en "\e[33m")
+  MAGENTA=$(echo -en "\e[35m")
+  WHITE=$(echo -en "\e[37m")
+  NORMAL=$(echo -en "\e[00m")
+  BOLD=$(echo -en "\e[01m")
+  UNDERLINE=$(echo -en "\e[04m")
+fi
 
 log_header() {
   printf "\n${BOLD}${MAGENTA}==========  %s  ==========${NORMAL}\n" "$@" >&2
